b2f799f5145860c490a6b18c7e19a5f9e4e5032a
Manuu500 <espartanmanu1122@gmail.com>
2025-03-05 18:53:54 +0100

    updated so_long

--- so_long.c
+++ so_long.c
@@ -6,17 +6,17 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:07:55 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2025/03/05 17:56:44 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2025/03/05 18:39:58 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx/mlx.h"
-#include <stdlib.h>
+# include <MLX42/MLX42.h>
+# include <stdlib.h>
 
 int	close_window(void	*mlx_ptr)
 {
 	(void)mlx_ptr;
-	exit(0);
+	mlx_close_window(0);
 	return(0);
 }
 
@@ -24,21 +24,21 @@ int key_press(int keycode, void *mlx_ptr)
 {
 	(void)mlx_ptr;
 	if (keycode == 65307)
-		exit (0);
+		mlx_close_window(0);
 	return(0);
 }
 
 int	main(void)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	mlx_t *mlx_ptr;
+	mlx_t *win_ptr;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 600, 400, "holis");
+	mlx_ptr = mlx_init(600, 400, "Prueba", 0);
+	if (!mlx_ptr)
+		return (EXIT_FAILURE);
 	
-	mlx_hook(win_ptr, 17, 0, close_window, mlx_ptr);
-	mlx_key_hook(win_ptr, key_press, mlx_ptr);
-
+	
 	mlx_loop(mlx_ptr);
-	return(0);
+	mlx_terminate(mlx_ptr);
+	return(EXIT_SUCCESS);
 }
