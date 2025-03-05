dd297313e1a819f616e048567400ef2ba11a0d49
Manuu500 <espartanmanu1122@gmail.com>
2025-03-05 20:00:01 +0100

    Added functionality to close the window with the escape key

--- main.c
+++ main.c
@@ -6,23 +6,38 @@
 /*   By: mruiz-ur <mruiz-ur@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 17:07:55 by mruiz-ur          #+#    #+#             */
-/*   Updated: 2025/03/05 19:07:36 by mruiz-ur         ###   ########.fr       */
+/*   Updated: 2025/03/05 19:59:03 by mruiz-ur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <MLX42/MLX42.h>
 # include <stdlib.h>
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+void	close_window(mlx_key_data_t keydata, void *param)
+{
+	t_vars *vars;
+
+	vars = (t_vars *)param;
+	if (keydata.key == MLX_KEY_ESCAPE)
+		mlx_close_window(vars->mlx);
+}
+
 int	main(void)
 {
-	mlx_t *mlx_ptr;
+	t_vars vars;
 
-	mlx_ptr = mlx_init(1200, 600, "Prueba", 0);
-	if (!mlx_ptr)
+	vars.mlx = mlx_init(1200, 600, "Prueba", 0);
+	if (!vars.mlx)
 		return (EXIT_FAILURE);
 	
-	
-	mlx_loop(mlx_ptr);
-	mlx_terminate(mlx_ptr);
+	mlx_key_hook(vars.mlx, close_window, &vars);
+		
+	mlx_loop(vars.mlx);
+	mlx_terminate(vars.mlx);
 	return(EXIT_SUCCESS);
 }
